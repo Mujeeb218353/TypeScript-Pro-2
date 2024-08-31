3fc0df0058968e4f9d5641a79a3ef114eb96248a
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2024-08-31 20:20:10 +0500

    Updates

---
+++ index.89fdde98.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("btn"),t=document.body;let o=!1;e?.addEventListener("click",()=>{o?(t.style.backgroundColor="",t.style.color="",e.innerText="Dark Mode"):(t.style.backgroundColor="black",t.style.color="white",e.innerText="Light Mode"),o=!o});
+//# sourceMappingURL=index.89fdde98.js.map
